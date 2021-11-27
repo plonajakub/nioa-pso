fc72468a8ce77910796a2b7963aca189e5939b02
Jakub Plona <plonajakub@gmail.com>
2021-11-27 15:10:39 +0100

    Add fake solutions

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,10 +1,16 @@
 #include "Solver.h"
 #include <vector>
 
+#include <thread>
+#include <chrono>
+using namespace std::chrono_literals;
+
 MKPSolution Solver::greedy(const MKPInstance &mkp_instance, const SolverSettings &settings) {
-    return {std::vector<bool>(mkp_instance.n_objects, false), -1};
+    std::this_thread::sleep_for(std::chrono::milliseconds(20 + static_cast<int>(static_cast<double>(rand()) / RAND_MAX * 20)));
+    return {std::vector<bool>(mkp_instance.n_objects, false), 1000 + static_cast<int>(static_cast<double>(rand()) / RAND_MAX * 1000)};
 }
 
 MKPSolution Solver::particle_swarm(const MKPInstance &mkp_instance, const SolverSettings &settings) {
-    return {std::vector<bool>(mkp_instance.n_objects, false), -1};
+    std::this_thread::sleep_for(std::chrono::milliseconds(50 + static_cast<int>(static_cast<double>(rand()) / RAND_MAX * 50)));
+    return {std::vector<bool>(mkp_instance.n_objects, false), 10000 + static_cast<int>(static_cast<double>(rand()) / RAND_MAX * 1000)};
 }
